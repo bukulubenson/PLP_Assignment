98938688b2f386969bf3cdfe5293f1c6389f6ab0
BensBenjamin <89865483+bukulubenson@users.noreply.github.com>
2023-02-15 17:05:01 +0300

    Initial commit

---
+++ DART/data_types_assignment.dart
@@ -0,0 +1,25 @@
+import 'dart:io';
+// the basic calculator program that takes the number entries from users
+// and return their sum,difference and product.
+
+//this is the function that takes in two integers from the user.
+void main(){
+  print('please enter your first number ');
+  int firstNumber = int.parse(stdin.readLineSync()!);
+  print('please enter the second number ');
+  int secondNumber = int.parse(stdin.readLineSync()!);
+  calculator(firstNumber, secondNumber);
+}
+
+//this function calculates the sum of the two entered numbers.
+dynamic calculator(int firstNumber, int secondNumber){
+  int sum = firstNumber + secondNumber;
+  int difference = firstNumber - secondNumber;
+  num product = firstNumber * secondNumber;
+  num quotient = firstNumber / secondNumber;
+  print('sum = $sum');
+  print('difference = $difference');
+  print('product = $product');
+  print('quotient = $quotient');
+}
+
